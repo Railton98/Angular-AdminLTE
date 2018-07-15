892231f2963f10d99d45412d8ccf611ea6fdcdcc
Railton98 <railtonleal98@gmail.com>
2018-07-15 00:09:29 -0300

    add to the versioning - subindo para o github

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AuthService } from './services/auth.service';
+import { LoginComponent } from './login/login.component';
+import { ProfileComponent } from './profile/profile.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+  ],
+  declarations: [
+    LoginComponent,
+    ProfileComponent
+  ],
+  providers: [
+    AuthService
+  ]
+})
+export class AuthModule {}
